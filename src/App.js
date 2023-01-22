69119912532bcf89fa6d409380acb2958522674e
Galaxy159 <braun.asaf@gmail.com>
2023-01-22 23:38:24 +0200

    final adjustments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./style/App.css";
 import Header from "./components/Header";
 import Component from "./components/Component";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
     <main className="container">
       <Header />
       <Component />
-      <Footer />
+      {/* <Footer /> */}
     </main>
   );
 }
