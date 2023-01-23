e800016a4ae0083c642e16a1df5aafb025206736
Galaxy159 <braun.asaf@gmail.com>
2023-01-23 12:52:48 +0200

    spelling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
       <div className="header">
         <h1 className="header__headline">Simple, traffic-based pricing</h1>
         <p className="header__text">
-          Sing-up for our 30 day trial. No credit card required.
+          Sign-up for our 30 day trial. No credit card required.
         </p>
       </div>
     </>
